5f84ad9fd3d44ad64552693048aba7b35a6777ba
Paul Liu <paul.liu.ubc@gmail.com>
2014-01-28 05:54:31 -0800

    pushing update to website

--- continuous_run.sh
+++ continuous_run.sh
@@ -10,11 +10,14 @@ while true; do
 	bdt_file=$RESULT_DIR/bdt-results/bdt-significance"$bdt_trial".txt
 	mlp_file=$RESULT_DIR/mlp-results/mlp-significance"$mlp_trial".txt
 	root -l -q "TMVAClassification.C(2)" > $bdt_file && root -l -q "TMVAClassification.C(1)" > $mlp_file
-	sleep 1
+	sleep 10
 
 	tail -n+3 $bdt_file > tmp.txt && mv tmp.txt $bdt_file
-	sleep 1
+	sleep 10
 	tail -n+3 $mlp_file > ttmp.txt && mv ttmp.txt $mlp_file
+	sleep 10
+
+	~/push_result_update.sh
 
 	mlp_trial=`expr $mlp_trial + 1`
 	bdt_trial=`expr $bdt_trial + 1`
