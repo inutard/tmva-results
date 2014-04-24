3b88337583458eda6c2541b16d369e65067ee5b2
Paul Liu <paul.liu.ubc@gmail.com>
2014-04-23 21:47:00 -0700

    pushing update to website

---
+++ continuous_run2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TMVA_DIR=$PWD/../tmva-studies-copy/lean_tmva/
+RESULT_DIR=$PWD
+bdt_trial=100
+mlp_trial=100
+
+cd $TMVA_DIR
+while true; do
+	bdt_file=$RESULT_DIR/bdt-results/bdt-significance"$bdt_trial".txt
+	mlp_file=$RESULT_DIR/mlp-results/mlp-significance"$mlp_trial".txt
+	./TMVAClassificationCpp 2 > $bdt_file && ./TMVAClassificationCpp 1 > $mlp_file
+	sleep 10
+
+	tail -n+3 $bdt_file > tmp.txt && mv tmp.txt $bdt_file
+	sleep 10
+	tail -n+3 $mlp_file > ttmp.txt && mv ttmp.txt $mlp_file
+	sleep 10
+
+	~/push_result_update.sh
+
+	mlp_trial=`expr $mlp_trial + 1`
+	bdt_trial=`expr $bdt_trial + 1`
+done
